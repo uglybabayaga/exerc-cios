609b152f566b4a33cf3abecada5aa5b09d33523a
uglybabayaga <uglybabayaga@gmail.com>
2024-12-06 18:23:10 -0300

    bla

---
+++ soma.py
@@ -0,0 +1,9 @@
+# Solicitar dois números ao usuário 
+num1 = float(input("digite o primeiro número: "))
+num2 = float(input ("digite o segundo número: "))
+
+# Somar os números
+resultado = num1 + num2
+
+# Exibir o resultado: 
+print (f"A soma de {num1} e {num2} é {resultado}.")
